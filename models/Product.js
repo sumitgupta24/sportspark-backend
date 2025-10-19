b744c246da0c13bbbc91a03f853886036f717bb0
Sumit Gupta <cricsumit24@gmail.com>
2025-10-19 16:30:04 +0530

    Refactor product schema: clean up image field comment

--- models/Product.js
+++ models/Product.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  image: { type: String }, // optional
+  image: { type: String }, 
   description: { type: String },
   price: { type: Number, required: true },
   stock: { type: Number, default: 0 },
