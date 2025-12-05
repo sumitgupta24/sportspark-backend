5e049c60be0bac20145dda0b39bfd4d59ba0916c
Sumit Gupta <161856895+sumitgupta24@users.noreply.github.com>
2025-12-05 22:13:33 +0530

    Clean up cartRoutes.js by removing comments
    Removed commented-out code for clarity.

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -10,14 +10,8 @@ const { protect } = require("../middleware/authMiddleware");
 
 // Add to cart
 router.post("/add", protect, addToCart);
-
-// Get user's cart
 router.get("/", protect, getCart);
-
-// ✅ Update item quantity
 router.put("/update", protect, updateCartItem);
-
-// Remove item from cart
 router.delete("/remove/:productId", protect, removeFromCart);
 
 module.exports = router;
